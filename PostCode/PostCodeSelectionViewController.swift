052c5f88eaa047408f9fa878ea89a2f014089191
Steve Jo <trilliwon@gmail.com>
2016-11-07 12:17:21 +0900

    indicator was added

--- PostCode/PostCodeSelectionViewController.swift
+++ PostCode/PostCodeSelectionViewController.swift
@@ -3,10 +3,11 @@ import WebKit
 
 
 
-class PostCodeSelectionViewController: UIViewController, WKScriptMessageHandler {
+class PostCodeSelectionViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
   
   var webView: WKWebView?
   
+  let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
   var postCode = ""
   var address = ""
   let unwind = "unwind"
@@ -22,6 +23,11 @@ class PostCodeSelectionViewController: UIViewController, WKScriptMessageHandler 
     
     self.webView = WKWebView(frame: .zero, configuration: config)
     self.view = self.webView!
+    self.webView?.navigationDelegate = self
+    
+    self.webView?.addSubview(indicator)
+    indicator.center.x = UIScreen.main.bounds.width/2
+    indicator.center.y = UIScreen.main.bounds.height/2
   }
   
   
@@ -34,6 +40,9 @@ class PostCodeSelectionViewController: UIViewController, WKScriptMessageHandler 
     
     let request = URLRequest(url: url)
     webView.load(request)
+    
+    self.webView?.navigationDelegate = self
+    indicator.startAnimating()
   }
 
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -46,12 +55,21 @@ class PostCodeSelectionViewController: UIViewController, WKScriptMessageHandler 
   }
   
   func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-    
     if let postCodData = message.body as? [String: Any] {
       postCode = postCodData["zonecode"] as? String ?? ""
       address = postCodData["addr"] as? String ?? ""
     }
     
     performSegue(withIdentifier: unwind, sender: nil)
+  }
+  
+  func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    indicator.startAnimating()
+  }
+  
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    indicator.stopAnimating()
   }
 }
